27a2c3b6334792d1b7dbb242d09a0699b17b62c2
Vasyl Liutikov <aleksey.gerasimov@postindustria.com>
2014-07-10 17:50:16 +0300

    change path to android manifest file

--- modules/bump_version_android_module.rb
+++ modules/bump_version_android_module.rb
@@ -5,12 +5,14 @@ class BumpVersionAndroidModule < BaseModule
   check_enabled!
   
   def self.run config
-    manifest_xml_file = config.runtime.project_dir + 'AndroidManifest.xml'
+    manifest_xml_file = config.runtime.project_dir + 'src/main/AndroidManifest.xml'
     info "Bumping Android version..."
+    info "#{manifest_xml_file}"
     begin
       ver = AndroidVersion.new manifest_xml_file
       ver.increment
       ver.write
+      info ver
       config.runtime.version = ver.version_name
       info "Bumping to versionCode:#{ver.version_code} versionName:#{ver.version_name}"
       if config.bump_version_android.push?
