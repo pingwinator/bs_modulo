a1f1dae42a0eb73971fd14b47b9c97022720ba9b
Vasyl Liutikov <vasyl.liutikov@postindustria.com>
2015-09-10 10:10:58 +0300

    remove enum uninstaller

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ bundle install
 sudo easy_install pip
 sudo pip install nose
 sudo pip install poster
-sudo pip uninstall enum
+#sudo pip uninstall enum
 sudo pip install enum34
 sudo pip install gevent
