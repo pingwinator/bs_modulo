98692d997f9f9675108e4922b5c92ddde6a80153
Alex Antonyuk <alex@antonyuk.me>
2016-04-21 10:29:09 +0300

    log info in build_module

--- modules/build_module.rb
+++ modules/build_module.rb
@@ -34,12 +34,15 @@ class BuildModule < BaseModule
     end
     
     #info command
+    info "Run command: #{command}"
     result = system command
     ## done building
     
     if config.build.build_with_gym?
       config.runtime.ipa_file = config.runtime.project_dir + "build/build.ipa"
       config.runtime.dsym_file = config.runtime.project_dir + "build/build.dSYM.zip"
+
+      puts "Paths: IPA: #{config.runtime.ipa_file}\n dSYM: #{config.runtime.dsym_file}"
     end
     
     hook! :build_complete
